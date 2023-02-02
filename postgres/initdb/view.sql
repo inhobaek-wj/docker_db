54791064ff5228c7f9635b416574b765f82c0cb6
jake <inhobaek.wj@gmail.com>
2023-02-02 17:37:31 +0900

    add postgres view

---
+++ postgres/initdb/view.sql
@@ -0,0 +1,8 @@
+
+-- CREATE VIEW "holidays" ---------------------------------------
+CREATE VIEW holidays AS
+  SELECT event_id AS holiday_id, title AS name, starts AS date, colors
+  FROM events
+  WHERE title LIKE '%Day%'
+  AND venue_id IS NULL;
+-- -------------------------------------------------------------
