99f114d67351b9f4f28a0e9e666027737d96ca0d
jake <inhobaek.wj@gmail.com>
2023-02-02 17:38:17 +0900

    add postgres RULE

--- postgres/initdb/view.sql
+++ postgres/initdb/view.sql
@@ -6,3 +6,14 @@ CREATE VIEW holidays AS
   WHERE title LIKE '%Day%'
   AND venue_id IS NULL;
 -- -------------------------------------------------------------
+
+
+-- CREATE RULE "update_holidays" ---------------------------------------
+CREATE RULE update_holidays AS ON UPDATE TO holidays DO INSTEAD
+  UPDATE events
+  SET title = NEW.name,
+      starts = NEW.date,
+      colors = NEW.colors
+  WHERE title = OLD.name;
+-- -------------------------------------------------------------
+
