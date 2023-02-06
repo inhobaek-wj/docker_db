25a1669c5d12a26600f2a018155548c9bfe519d0
inho <inhobaek.wj@gmail.com>
2023-02-06 09:26:55 +0900

    add movie tables

--- postgres/initdb/init.sql
+++ postgres/initdb/init.sql
@@ -59,6 +59,41 @@ CREATE TABLE logs (
 -- -------------------------------------------------------------
 
 
+-- CREATE TABLE "genres" ---------------------------------------
+CREATE TABLE genres (
+  name text UNIQUE,
+  position integer
+);
+-- -------------------------------------------------------------
+
+
+-- CREATE TABLE "movies" ---------------------------------------
+CREATE TABLE movies (
+  movie_id SERIAL PRIMARY KEY,
+  title text,
+  genre cube
+);
+-- -------------------------------------------------------------
+
+
+-- CREATE TABLE "actors" ---------------------------------------
+CREATE TABLE actors (
+  actor_id SERIAL PRIMARY KEY,
+  name text
+);
+-- -------------------------------------------------------------
+
+
+-- CREATE TABLE "movies_actors" ---------------------------------------
+CREATE TABLE movies_actors (
+  movie_id integer REFERENCES movies NOT NULL,
+  actor_id integer REFERENCES actors NOT NULL,
+  UNIQUE (movie_id, actor_id)
+);
+
+-- -------------------------------------------------------------
+
+
 -- Dump data of "countries" -----------------------------------
 BEGIN;
 
@@ -109,4 +144,24 @@ CREATE INDEX events_title ON events USING hash (title);
 
 -- CREATE INDEX "events_starts" --------------------
 CREATE INDEX events_starts ON events USING btree (starts);
+-- -------------------------------------------------------------
+
+
+-- CREATE INDEX "movies_actors_movie_id" --------------------
+CREATE INDEX movies_actors_movie_id ON movies_actors (movie_id);
+-- -------------------------------------------------------------
+
+
+-- CREATE INDEX "movies_actors_actor_id" --------------------
+CREATE INDEX movies_actors_actor_id ON movies_actors (actor_id);
+-- -------------------------------------------------------------
+
+
+-- CREATE INDEX "movies_genres_cube" --------------------
+CREATE INDEX movies_genres_cube ON movies USING gist (genre);
+-- -------------------------------------------------------------
+
+
+-- CREATE INDEX "movies_title_pattern" --------------------
+CREATE INDEX movies_title_pattern ON movies (lower(title) text_pattern_ops);
 -- -------------------------------------------------------------
